f9764a46cae539d2c5cdc2c595b97ab7b3eb631c
Cybervtx <149208626+Cybervtx@users.noreply.github.com>
2024-10-26 20:18:02 +0000

    feat: :sparkles: add bd

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 # Importação
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Modelagem
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    return data
 
 # Definir uma rota raiz (pagina inicial)
 @app.route('/')
