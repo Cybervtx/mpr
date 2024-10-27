3412b2d1712aea862ace319cf8a05713aeebe8f7
Cybervtx <149208626+Cybervtx@users.noreply.github.com>
2024-10-27 14:20:33 +0000

    feat: :sparkles: route get one product

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 
-# Modelagem
+# Modelagem do banco de dados
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False, unique=True)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+
+# Definir uma rota raiz (pagina inicial)
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
 
 # Adicionando produto ao banco de dados
 @app.route('/api/products/add', methods=['POST'])
@@ -29,6 +35,7 @@ def add_product():
             return jsonify({"message": "Produto Já existe"}), 409
     return jsonify({"message": "Invalid product data"}), 400
 
+
 # Excluindo produto do banco de dados
 @app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
@@ -39,10 +46,18 @@ def delete_product(product_id):
         return jsonify({"message": "Produto deletado com sucesso"}), 200
     return jsonify({"message": "Produto nao encontrado"}), 404
 
-# Definir uma rota raiz (pagina inicial)
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        }), 200
+    return jsonify({"message": "Produto nao encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
